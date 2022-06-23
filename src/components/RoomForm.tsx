b484de2c5520b31b434929cb8ea4c04f28196543
PathonScript <pathonscript@gmail.com>
2022-06-23 19:43:56 +0700

    InputValue joining

--- src/components/RoomForm.tsx
+++ src/components/RoomForm.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link';
 
-
-let keycode: number = Math.floor(Math.random()* 90000) + 10000;;
+let keycode: number = Math.floor(Math.random()* 90000) + 10000;
 function createRoom(){
 	// Do backend stuff here I think
 
@@ -12,11 +11,18 @@ function createRoom(){
 
 
 function RoomForm() {
+	const [inputValue, setInputValue] = useState('');
+
   return (
     <>
 			<div className="my-2 flex flex-row">
-				<input type="number" className="text-3xl w-56 h-24 bg-slate-700 rounded-lg p-2 text-center" placeholder="Room Code" pattern="\d*" maxLength={5}/>
-				<button className="button ml-2">Join</button>
+				<input type="number" className="text-3xl text-white w-56 h-20 bg-slate-700 rounded-lg p-2 text-center" placeholder="Room Code" pattern="\d*" maxLength={5} value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+				
+				{inputValue ? 
+					<Link href={`/${inputValue}`}><a className="button ml-2">Join</a></Link> 
+					: 
+					<button className="button">Join</button>
+				}
 			</div>
 			<Link href={`/${keycode}`}><a className="button" onClick={createRoom}>Create Room</a></Link>
     </>
