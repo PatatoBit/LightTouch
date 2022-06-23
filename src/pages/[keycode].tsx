2daf93fc71b6cc2b4a8140b0948a2151249c317b
teely44y <ju.tannatee2543@gmail.com>
2022-06-23 23:16:36 +0700

    feat: extract component function

--- src/pages/[keycode].tsx
+++ src/pages/[keycode].tsx
@@ -1,25 +1,68 @@
-import React from 'react'
+import type { GetServerSideProps } from "next";
+import { useState, useEffect } from "react";
+import { useRouter, NextRouter } from "next/router";
+import Link from "next/link";
 
-import { useRouter } from 'next/router';
-import Link from 'next/link';
+import middleware from "../middlewares/middleware";
 
-import Ring from '../components/Ring'
+import Ring from "../components/Ring";
 
-function Lobby() {
-	const router = useRouter();
+import { io, Socket } from "socket.io-client";
+let socket: Socket;
+
+export default function Lobby() {
+	const router: NextRouter = useRouter();
 	const { keycode } = router.query;
+
+	const [light, setLight] = useState(false);
+
+	const mouseEnter = async () => {
+		socket.emit(`${keycode} light`, true);
+		setLight(true);
+
+		return;
+	};
+
+	const mouseLeave = async () => {
+		socket.emit(`${keycode} light`, false);
+		setLight(false);
+
+		return;
+	};
+
+	useEffect((): any => {
+		socket = io();
+
+		socket.on(`${keycode} light`, (status) => {
+			setLight(status[0]);
+		});
+
+		if (socket) return () => socket.disconnect();
+	}, [keycode]);
 
 	return (
 		<>
 			<div className="flex flex-col w-full h-screen items-center justify-center bg-slate-800">
 				<h1 className="text-5xl text-yellow-300">Light Touch</h1>
-				<div className="text-2xl mt-2 text-yellow-300">Room ID: { keycode }</div>
+				<div className="text-2xl mt-2 text-yellow-300">Room ID: {keycode}</div>
 
-				<Ring />
-				<Link href="/"><a className="button">Leave</a></Link>
+				<Ring
+					isHovering={light}
+					onMouseEnter={mouseEnter}
+					onMouseLeave={mouseLeave}
+				/>
+				<Link href="/">
+					<a className="button">Leave</a>
+				</Link>
 			</div>
 		</>
-	)
+	);
 }
 
-export default Lobby
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+	await middleware.run(req, res);
+
+	return {
+		props: {},
+	};
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,59 @@
+import { GetServerSideProps } from "next";
+import { useState, useEffect } from "react";
+
+import middleware from "../middlewares/middleware";
+
 import Ring from "../components/Ring";
+import RoomForm from "../components/RoomForm";
 
-import RoomForm from '../components/RoomForm'
+import { io, Socket } from "socket.io-client";
 
-import useSocket from "../hooks/useSocket";
-
+let socket: Socket;
 
 export default function Home() {
-	useSocket("test", () => {
-		console.log("test");
-	});
+	const [light, setLight] = useState(false);
+
+	const mouseEnter = async () => {
+		socket.emit("public light", true);
+		setLight(true);
+
+		return;
+	};
+
+	const mouseLeave = async () => {
+		socket.emit("public light", false);
+		setLight(false);
+
+		return;
+	};
+
+	useEffect((): any => {
+		socket = io();
+
+		socket.on("public light", (status) => {
+			setLight(status[0]);
+		});
+
+		if (socket) return () => socket.disconnect();
+	}, []);
+
 	return (
 		<div className="flex flex-col w-full h-screen items-center justify-center bg-slate-800">
 			<h1 className="text-5xl text-yellow-300">Light Touch</h1>
-			<Ring />
+			<Ring
+				isHovering={light}
+				onMouseEnter={mouseEnter}
+				onMouseLeave={mouseLeave}
+			/>
 			<RoomForm />
 		</div>
 	);
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+	await middleware.run(req, res);
+
+	return {
+		props: {},
+	};
+};
