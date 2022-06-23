1d890d0ad32c2b99c3cbfa07cb097609a0222df4
PathonScript <pathonscript@gmail.com>
2022-06-23 14:45:02 +0700

    Removed Comments

--- src/components/Ring.tsx
+++ src/components/Ring.tsx
@@ -1,16 +1,9 @@
 import React, { useState } from 'react'
 
-
-
 export default function Ring() {
   const [ isHovered, setHovering ] = useState(false)
   const onMouseEnter = () => setHovering(true);
   const onMouseLeave = () => setHovering(false);
-
- 
-  // return (
-  //   <div className="my-10 border-4 border-yellow-300 rounded-full w-80 h-80 hover:shadow-yellow-200 hover:shadow-2xl hover:bg-yellow-200 hover:border-yellow-200 transition-all"></div>
-  // )
 
   if ( isHovered ) {
     return (
