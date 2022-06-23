391e16c0e266030b0623edb34fd07b3b380a9ba7
PathonScript <pathonscript@gmail.com>
2022-06-23 12:42:37 +0700

    Separated to components, Room Code, horver border color

---
+++ src/components/Ring.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Ring() {
+  return (
+    <div className="my-10 border-4 border-yellow-300 rounded-full w-80 h-80 hover:shadow-yellow-200 hover:shadow-2xl hover:bg-yellow-200 hover:border-yellow-200 transition-all"></div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from 'next'
+import Ring from '../components/Ring';
 
 const Home: NextPage = () => {
   return (
     <>
       <div className="flex flex-col w-full h-screen items-center justify-center bg-slate-800">
-        <h1 className="text-5xl text-yellow-300">
-          Light Touch
-        </h1>
+        <h1 className="text-5xl text-yellow-300">Light Touch</h1>
 
-        <div className="mt-10 border-4 border-yellow-300 rounded-full w-80 h-80 hover:shadow-yellow-200 hover:shadow-2xl hover:bg-yellow-200 transition-all"></div>
+        <Ring />
+
+        <h1 className="text-3xl text-yellow-300">Room Code</h1>
+        <h1 className="text-4xl text-yellow-300">1234</h1>
+
       </div>
     </>
   )
