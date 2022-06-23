f18c4aa7353ea7a3a4b464bf92c9c354779a1828
PathonScript <pathonscript@gmail.com>
2022-06-23 18:06:44 +0700

    Extract button class to @apply

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -15,9 +15,9 @@ function Room() {
     <>
 			<div className="my-2 flex flex-row">
 				<input type="number" className="text-3xl w-56 h-24 bg-slate-700 rounded-lg p-2 text-center" placeholder="Room Code" pattern="\d*" maxLength={5}/>
-				<button className="text-2xl text-yellow-300 p-2 border-4 border-yellow-300 rounded-lg ml-2">Join</button>
+				<button className="button ml-2">Join</button>
 			</div>
-      <button className="text-3xl text-yellow-300 font-bold border-4 border-yellow-300 rounded-lg p-2" onClick={createRoom}>Create Room</button>
+      <button className="button" onClick={createRoom}>Create Room</button>
     </>
   )
 }
