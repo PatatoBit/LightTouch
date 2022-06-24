36fafebe524cbd2ceaacc0b62e9608dd6e10c87e
PathonScript <pathonscript@gmail.com>
2022-06-24 10:38:58 +0700

    Update RoomForm

--- src/components/RoomForm.tsx
+++ src/components/RoomForm.tsx
@@ -3,18 +3,15 @@ import Link from 'next/link';
 
 let keycode: number = Math.floor(Math.random()* 90000) + 10000;
 function createRoom(){
-	// Do backend stuff here I think
-
 	// Create a random keycode
 	keycode = Math.floor(Math.random()* 90000) + 10000;
 }
-
 
 function RoomForm() {
 	const [inputValue, setInputValue] = useState('');
 
   return (
-    <>
+		<div className="flex flex-col w-74">
 			<div className="my-2 flex flex-row">
 				<input type="number" className="input" placeholder="Room Code" pattern="\d*" maxLength={5} value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
 				
@@ -24,8 +21,8 @@ function RoomForm() {
 					<button className="button ml-2">Join</button>
 				}
 			</div>
-			<Link href={`/${keycode}`}><a className="button" onClick={createRoom}>Create Room</a></Link>
-    </>
+			<Link href={`/${keycode}`}><a className="button text-center" onClick={createRoom}>Create Room</a></Link>
+		</div>
   )
 }
 
